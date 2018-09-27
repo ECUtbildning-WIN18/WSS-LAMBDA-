4bbc13d93981485e0ff38a5b2a0ee00424a548fa
Wade Tamim <w.tamim@hotmail.com>
2018-09-27 13:04:21 +0200

    Add project files.

---
+++ WSS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WSS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
